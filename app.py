f4cbcb6ce4d6a50d85bff34a5c2c34c51004d945
Cheeky <sudaksh77@gmail.com>
2024-12-02 12:42:44 +1100

    Made only the number text is bold

--- app.py
+++ app.py
@@ -4,4 +4,4 @@ from helpers import numberToWords
 
 st.header("Number to Text", divider='blue')
 inputNumber = st.number_input("Enter a number", format="%.2f", step=1.)
-st.markdown("# The current number is :blue[{}]".format(numberToWords(inputNumber)))
+st.markdown("# **The current number is** :blue[{}]".format(numberToWords(inputNumber)))
