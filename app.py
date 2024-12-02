b1df87560e2452e33dcba4cb28b51046fdf8692b
Cheeky <sudaksh77@gmail.com>
2024-12-02 12:34:04 +1100

    Increased step size to 1

--- app.py
+++ app.py
@@ -3,5 +3,5 @@ import streamlit as st
 from helpers import numberToWords
 
 st.header("Number to Text", divider='blue')
-inputNumber = st.number_input("Enter a number", format="%.2f")
-st.markdown("The current number is :blue[{}]".format(numberToWords(inputNumber)))
+inputNumber = st.number_input("Enter a number", format="%.2f", step=1.)
+st.markdown("# The current number is :blue[{}]".format(numberToWords(inputNumber)))
